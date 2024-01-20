957c7e443eb19689e323230e9cd6483e3da33b60
Michelle Morrow <michelle.allyson.morrow@gmail.com>
2024-01-19 19:41:16 -0500

    Edit to Deck.js to map over decks and add buttons--need to troubleshoot

--- src/Layout/Deck.js
+++ src/Layout/Deck.js
@@ -5,28 +5,33 @@ import { Link, useHistory } from "react-router-dom";
 
 import Home from "./Home";
 import { readDeck , deleteDeck } from "../utils/api";
+import { TrashIcon } from '@heroicons/react/24/solid';
+import { BookmarkIcon } from '@heroicons/react/24/solid';
+import { PlusIcon } from '@heroicons/react/16/solid';
+import { PencilSquareIcon} from '@heroicons/react/16/solid';
+
 
 //path: "/decks/:deckId"
 
 const Deck = () => {
+    const [decks, setDecks] = useState([]);
+    const history = useHistory();
 
-
-const [deck, setDeck] = useState([]);
-const history = useHistory();
-
-const handleDelete = async (deckId) => {
-    const result = window.confirm("Delete this deck? You will not be able to recover it.");
-    if (result) {
-        await deleteDeck(deckId);
-        history.go(0);
-    }   
-}
+    const handleDelete = async (deckId) => {
+        const result = window.confirm(
+            "Delete this deck? You will not be able to recover it."
+        );
+        if (result) {
+            await deleteDeck(deckId);
+            history.go(0);
+        }
+    };
 
     useEffect(() => {
         async function loadDeck() {
             try {
                 const response = await readDeck();
-                setDeck(response);
+                setDecks(response);
             } catch (error) {
                 console.log(error);
             }
@@ -34,23 +39,54 @@ const handleDelete = async (deckId) => {
         loadDeck();
     }, []);
 
-//need to map over decks--see Home.js for example
-
     return (
-        <>
-            <div>
+        <div>
+        {decks.map((deck) => (
+            <div key={deck.id}>
                 <h1>Deck</h1>
+                <nav aria-label="breadcrumb">
+                    <ol className="breadcrumb">
+                        <li className="breadcrumb-item">
+                            <Link to="/"> Home / </Link>
+                        </li>
+                        <li>{deck.name}</li>
+                    </ol>
+                </nav>
+                <div>
+                    <h2>{deck.name}</h2>
+                    <p>{deck.description}</p>
+                <div>
+                    <Link
+                        to={`/decks/${deck.id}/edit`}
+                        className="btn btn-secondary btn-sm"
+                    >
+                        <PencilSquareIcon /> Edit 
+                    </Link>
+                    <Link
+                        to={`/decks/${deck.id}/study`}
+                        className="btn btn-primary btn-sm"
+                    >
+                        <BookmarkIcon /> Study
+                    </Link>
+                    <Link
+                        to={`/decks/${deck.id}/cards/new`}
+                        className="btn btn-primary btn-sm"
+                    >
+                        <PlusIcon /> Add Cards
+                    </Link>
+                    <button
+                        type="button"
+                        className="btn btn-danger btn-sm"
+                        onClick={() => handleDelete(deck.id)}
+                    >
+                        <TrashIcon /> Delete
+                    </button>
+                </div>
             </div>
-            <nav aria-label="breadcrumb">
-                <ol className="breadcrumb">
-                    <li className="breadcrumb-item">
-                        <Link to="/"> Home / </Link>
-                    </li>
-                    {/* <li>{Deck.id}</li> */}
-                </ol>
-            </nav>
-        </>
+        </div>
+    ))}
+    </div>
     );
-}
+};
 
 export default Deck;
