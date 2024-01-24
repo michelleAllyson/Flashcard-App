67f71904371195410aaedeb776a60971fdef8ddf
Michelle Morrow <michelle.allyson.morrow@gmail.com>
2024-01-24 10:29:14 -0500

    Deck.js update--meets functionality for all but edit card button; Still needs styling

--- src/Layout/Deck.js
+++ src/Layout/Deck.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
 
 //utils
-import { readDeck, deleteDeck, updateDeck, deleteCard, readCard } from "../utils/api";
+import { readDeck, deleteDeck, deleteCard, readCard } from "../utils/api";
 
 //libraries for icons
 import { TrashIcon } from '@heroicons/react/24/solid';
@@ -12,38 +12,14 @@ import { PencilSquareIcon } from '@heroicons/react/16/solid';
 
 
 //The Deck screen has the following features:
-    // The path to this screen should include the deckId (i.e., /decks/:deckId). 
-        //---------!!!!!DONE!!!!!!!!: EX: http://localhost:3000/decks/2
-    // You must use the readDeck() function from src/utils/api/index.js to load the existing deck.
-        //---------!!!!!DONE!!!!!!!!:
-    // There is a breadcrumb navigation bar with a link to home / followed by the name of the deck (e.g., Home/React Router).
-        //---------!!!!!DONE!!!!!!!!:
-    // The screen includes the deck name (e.g., "React Router") and deck description (e.g., "React Router is a collection of navigational components that compose declaratively in your application").
-        //---------!!!!!DONE!!!!!!!!:
-
-    // The screen includes Edit, Study, Add Cards, and Delete buttons. Each button takes the user to a different destination, as follows:
-        // | Button Clicked | Destination |
-        // | -------------- | ---------------------------------------------------------------------------------------------- |
-        // | Edit | Edit Deck Screen |
-        // | Study | Study screen |
-        // | Add Cards | Add Card screen |
-        // | Delete | Shows a warning message before deleting the deck]( See the "Delete Card Prompt" section below) |
-
-
     // Each card in the deck:
-        // Is listed on the page under the "Cards" heading.
-            //--DONE, but needs formatting
-        // Shows a question and the answer to the question.
-            //--DONE, but needs formatting
         // Has an Edit button that takes the user to the Edit Card screen when clicked.
             //--takes user to Edit screen, but Card is not loaded here yet...
-        // Has a Delete button that allows that card to be deleted.
-            //--Button displayed but not yet functional
-        // Delete Card Prompt
-        // When the user clicks the Delete button associated with a card, a warning message is shown and the user can click OK or Cancel. If the user clicks OK, the card is deleted.
 
-        // You can use window.confirm() to create the modal dialog shown in the screenshot below
+//ALL Deck.js requirements are met, except for the Edit button on each card---need to come back to this after EditCard.js is finished
 
+//Still needs styling
+        
 
 
 function Deck() {
@@ -86,14 +62,19 @@ function Deck() {
     loadCards();
   }, [cardId]); 
 
-  const handleCardDelete = async ({ target }) => {
-    const confirm = window.confirm("Delete this card? You will not be able to recover it.");
-    if (confirm) {
-        deleteCard(target.value)
-        .then(updateDeck(deckId))
-        .then(window.location.reload());
+  async function handleDeleteCard(cardId) {
+    const result = window.confirm(
+      "Delete this card? You will not be able to recover it."
+    );
+    if (result) {
+      await deleteCard(cardId);
+      history.go(0);
     }
-}
+  }
+
+  async function handleEditCard(cardId) {
+    history.push(`/decks/${deckId}/cards/${cardId}/edit`);
+  }
 
   return (
     <div>
@@ -167,7 +148,7 @@ function Deck() {
                   <button
                     type="button"
                     className="btn btn-danger btn-sm"
-                    onClick={() => handleCardDelete(card.id)}
+                    onClick={() => handleDeleteCard(card.id)}
                     >
                     <TrashIcon /> Delete
                   </button>
